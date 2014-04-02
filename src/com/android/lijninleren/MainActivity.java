8ae4123923de10e04c1aafabefc20201c24d2157
SuperJoot <Joost_van_eeden@hotmail.com>
2014-04-02 21:05:54 +0200

    Testing Joost 2

--- src/com/android/lijninleren/MainActivity.java
+++ src/com/android/lijninleren/MainActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//Testing Joost
+//Testing Joost 123
 package com.android.lijninleren;
 
 import java.util.Locale;
