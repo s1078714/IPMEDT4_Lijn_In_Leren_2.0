3800b0ea4ad68b70580f260f2ca8d8029136ec37
s1078714 <s1078714@student.hsleiden.nl>
2014-04-07 21:48:44 +0200

    MainActivity opnieuw toevoegen, na probleem Rebase

---
+++ src/com/android/lijninleren/MainActivity.java
@@ -0,0 +1,114 @@
+package com.android.lijninleren;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends FragmentActivity {
+	static String url = "http://145.101.81.229:80/";
+
+	private String[] data;
+	private String[] fragments;
+
+	private ActionBarDrawerToggle mDrawerToggle;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mTitle = mDrawerTitle = getTitle();
+		data = getResources().getStringArray(R.array.menu_array);
+		fragments = getResources().getStringArray(R.array.fragment_array);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActionBar().getThemedContext(), android.R.layout.simple_list_item_1, data);
+
+		final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		final ListView navList = (ListView) findViewById(R.id.drawer);
+		navList.setAdapter(adapter);
+		navList.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, final int pos,long id){
+				if ( pos == 4 )
+				{
+					finish();
+				}
+				drawer.setDrawerListener( new DrawerLayout.SimpleDrawerListener(){
+					@Override
+					public void onDrawerClosed(View drawerView){
+						super.onDrawerClosed(drawerView);
+						FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+						tx.replace(R.id.main, Fragment.instantiate(MainActivity.this, fragments[pos]));
+						tx.commit();
+					}
+				});
+				drawer.closeDrawer(navList);
+			}
+		});
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		mDrawerToggle = new ActionBarDrawerToggle(
+				this,                  /* host Activity */
+				drawer,         /* DrawerLayout object */
+				R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+				R.string.drawer_open,  /* "open drawer" description for accessibility */
+				R.string.drawer_close  /* "close drawer" description for accessibility */
+				) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+		};
+		drawer.setDrawerListener(mDrawerToggle);
+
+		FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+		tx.replace(R.id.main,Fragment.instantiate(MainActivity.this, fragments[0]));
+		tx.commit();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+}
