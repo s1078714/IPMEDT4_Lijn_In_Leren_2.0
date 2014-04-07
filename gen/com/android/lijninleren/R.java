de4d41d6d32ea0713318cd30403acea109284f9d
s1078714 <s1078714@student.hsleiden.nl>
2014-04-07 20:32:01 +0200

    oude commit

--- gen/com/android/lijninleren/R.java
+++ gen/com/android/lijninleren/R.java
@@ -9,12 +9,9 @@ package com.android.lijninleren;
 
 public final class R {
     public static final class array {
-        public static final int menu_array=0x7f060000;
+        public static final int menu_array=0x7f050000;
     }
     public static final class attr {
-    }
-    public static final class color {
-        public static final int wit=0x7f040000;
     }
     public static final class drawable {
         public static final int action_search=0x7f020000;
@@ -31,14 +28,14 @@ public final class R {
         public static final int venus=0x7f02000b;
     }
     public static final class id {
-        public static final int action_websearch=0x7f090007;
-        public static final int content=0x7f090004;
-        public static final int content_frame=0x7f090001;
-        public static final int drawer_layout=0x7f090000;
-        public static final int iid=0x7f090005;
-        public static final int left_drawer=0x7f090002;
-        public static final int name=0x7f090006;
-        public static final int text1=0x7f090003;
+        public static final int action_websearch=0x7f080007;
+        public static final int content=0x7f080004;
+        public static final int content_frame=0x7f080001;
+        public static final int drawer_layout=0x7f080000;
+        public static final int iid=0x7f080005;
+        public static final int left_drawer=0x7f080002;
+        public static final int name=0x7f080006;
+        public static final int text1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -48,18 +45,18 @@ public final class R {
         public static final int list_item=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_websearch=0x7f050003;
-        public static final int app_name=0x7f050000;
-        public static final int app_not_available=0x7f050004;
-        public static final int drawer_close=0x7f050002;
-        public static final int drawer_open=0x7f050001;
-        public static final int imageDescription=0x7f050005;
+        public static final int action_websearch=0x7f040003;
+        public static final int app_name=0x7f040000;
+        public static final int app_not_available=0x7f040004;
+        public static final int drawer_close=0x7f040002;
+        public static final int drawer_open=0x7f040001;
+        public static final int imageDescription=0x7f040005;
     }
     public static final class style {
-        public static final int AppBaseTheme=0x7f070000;
-        public static final int AppTheme=0x7f070001;
+        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppTheme=0x7f060001;
     }
 }
