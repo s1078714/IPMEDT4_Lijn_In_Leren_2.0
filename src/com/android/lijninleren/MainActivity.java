8259392f9aa7940bf6de5d4a320a90311a0ea979
s1078714 <s1078714@student.hsleiden.nl>
2014-04-07 21:13:57 +0200

    Launch nieuwe app. (Bestaande uit 1 activity en diverse fragments)

--- src/com/android/lijninleren/MainActivity.java
+++ src/com/android/lijninleren/MainActivity.java
@@ -1,244 +1,114 @@
-/*
- * Copyright 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.lijninleren;
 
-import java.util.Locale;
-
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.SearchManager;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
+public class MainActivity extends FragmentActivity {
+	static String url = "http://145.101.81.229:80/";
 
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-    private String[] mMenuTitles;
-   
-    private AllItemsActivity allItems;
+	private String[] data;
+	private String[] fragments;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private ActionBarDrawerToggle mDrawerToggle;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
 
-        mTitle = mDrawerTitle = getTitle();
-        mMenuTitles = getResources().getStringArray(R.array.menu_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mMenuTitles));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		mTitle = mDrawerTitle = getTitle();
+		data = getResources().getStringArray(R.array.menu_array);
+		fragments = getResources().getStringArray(R.array.fragment_array);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActionBar().getThemedContext(), android.R.layout.simple_list_item_1, data);
 
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* Host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* Nav drawer image to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
-                ) {
-            public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
+		final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		final ListView navList = (ListView) findViewById(R.id.drawer);
+		navList.setAdapter(adapter);
+		navList.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, final int pos,long id){
+				if ( pos == 4 )
+				{
+					finish();
+				}
+				drawer.setDrawerListener( new DrawerLayout.SimpleDrawerListener(){
+					@Override
+					public void onDrawerClosed(View drawerView){
+						super.onDrawerClosed(drawerView);
+						FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+						tx.replace(R.id.main, Fragment.instantiate(MainActivity.this, fragments[pos]));
+						tx.commit();
+					}
+				});
+				drawer.closeDrawer(navList);
+			}
+		});
 
-            public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
 
-        if (savedInstanceState == null) {
-            selectItem(1);
-        }
-    }
+		mDrawerToggle = new ActionBarDrawerToggle(
+				this,                  /* host Activity */
+				drawer,         /* DrawerLayout object */
+				R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+				R.string.drawer_open,  /* "open drawer" description for accessibility */
+				R.string.drawer_close  /* "close drawer" description for accessibility */
+				) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+		};
+		drawer.setDrawerListener(mDrawerToggle);
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
+		FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+		tx.replace(R.id.main,Fragment.instantiate(MainActivity.this, fragments[0]));
+		tx.commit();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        switch(item.getItemId()) {
-        case R.id.action_websearch:
-            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            } else {
-                Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
-            }
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-            
-            if ( position == 0 ) /* menu item "Zoeken" */
-            {
-            	// search code https://developer.android.com/training/search/search.html
-            	// de zoekmachine die nu in de app zit verbind door naar google/zoekt op internet.
-            	// dat moeten we niet hebben, we willen een zoekfunctie die intern zoekt
-            	String message = "U klikte op " + getActionBar().getTitle() + "!";
-            	Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-            }
-            
-            if ( position == 1 ) /* menu item "Browsen" */
-            {
-            	// Database inlezen.
-            	// ListView gebruiken om uitdraai van query in weer te geven.
-            	// Nieuwe query uitvoeren op basis van geklikt item uit ListView.
-            	Intent i = new Intent(MainActivity.this, AllItemsActivity.class);
-                startActivity(i);
-                
-                String message = "U klikte op Browsen!";
-            	Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-            }
-            
-            if ( position == 2 ) /* menu item "Favorieten" */
-            {
-            	// gebruik maken van "arrayFavorieten"
-            	// ListView gebruiken om array te vullen met de favorieten.
-            }
-            
-            if ( position == 3 ) /* menu item "Verversen" */
-            {
-            	// get-statement om tabel van server te halen
-            	// deze functie kan nog geschrapt worden. afhankelijk wat gunnar te weten komt
-            	// over de connectiviteit tussen app en server tijdens het navigeren.
-//            	dbHelper.onUpgrade( db, dbHelper.DATABASE_VERSION, dbHelper.DATABASE_VERSION ++);
-            }
-            
-            if ( position == 4 ) /* menu item "Instellingen" */
-            {
-            	// Mocht deze optie niet gebruikt worden in de app
-            	// dan dient menu item "Afsluiten" hier voor aangepast
-            	// te worden ( position == ? )
-            }
-        
-            if ( position == 5 ) /* menu item "Afsluiten" */
-            {
-            	finish();
-            }
-        }
-        
+		return super.onOptionsItemSelected(item);
+	}
 
-        
-    }
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
 
-    private void selectItem(int position) {
-        Fragment fragment = new MenuFragment();
-        Bundle args = new Bundle();
-        args.putInt(MenuFragment.ARG_MENU_NUMBER, position);
-        fragment.setArguments(args);
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mMenuTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    public static class MenuFragment extends Fragment {
-        public static final String ARG_MENU_NUMBER = "menu_number";
-
-        public MenuFragment() {
-        	// Lege constructor nodig voor fragment subclasses
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
-            int i = getArguments().getInt(ARG_MENU_NUMBER);
-            String menu = getResources().getStringArray(R.array.menu_array)[i];
-
-            int menuId = getResources().getIdentifier(menu.toLowerCase(Locale.getDefault()),
-                            "drawable", getActivity().getPackageName());
-            ((ImageView) rootView.findViewById(R.id.content)).setImageResource(menuId); // .setImageResource vervangen door setContentResource oid.
-            getActivity().setTitle(menu);
-            return rootView;
-        }
-    }
-    
-    public void finish() 
-    {
-        super.finish();
-    }
 }
